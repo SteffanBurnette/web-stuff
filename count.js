2c05e7d175c65a7d9239602a6125ec7836befdeb
SteffanBurnette <burnettesteffansep7@gmail.com>
2026-02-02 17:22:04 -0500

    test

--- count.js
+++ count.js
@@ -2,9 +2,12 @@
 
 //let count = 0
 //console.log(count)
+//document is an object
 
 let count = 0
 let countEl = document.getElementById("count-el")
+let entries = ""
+let previousEntries = document.getElementById("entries")
 
 function increment(){
     console.log("The button was clicked")
@@ -12,4 +15,11 @@ function increment(){
     countEl.innerText = count
     console.log(count)
 
+}
+
+//A function that logs the count when it's called
+function save(){
+    console.log(count)
+    entries += String(count) + " -> "
+    previousEntries.innerText = entries 
 }
