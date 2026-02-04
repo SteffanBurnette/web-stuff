c0f0f204b9c7b862ab7fdc71bad26c6e351939c9
SteffanBurnette <burnettesteffansep7@gmail.com>
2026-02-04 13:19:57 -0500

    test

--- blackJack.js
+++ blackJack.js
@@ -1,6 +1,7 @@
 let firstCard = 6
 let secondCard = 9
-let sum = firstCard + secondCard
+let cards  = [firstCard, secondCard]
+let sum = cards[0] + cards[1]
 let hasBlackJack = false
 let isAlive = true
 let message = ""
@@ -10,12 +11,16 @@ let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function startGame(){
+    renderGame()
+}
+
+function renderGame(){
 
     if (sum < 21){
         message = "Do you want to draw a new card?"
     }
     else if(sum === 21){
-        message = "You've git black jack!"
+        message = "You've got black jack!"
         hasBlackJack = true
     }
     else if(sum > 21){
@@ -25,8 +30,19 @@ function startGame(){
 
     console.log(message)
     messageEl.textContent = message
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    for(let card of cards){
+        cardsEl.textContent += " " + card
+    }
+    //cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
     sumEl.textContent = "Sum: " + sum 
 }
 
 
+function newCard(){
+     //firstCard = 10
+    // secondCard = 11
+    cards.push(6)
+    sum += cards[cards.length - 1]
+    cardsEl.textContent += " " + cards[cards.length - 1]
+    sumEl.textContent = "Sum: " + sum 
+}
